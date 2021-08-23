5b322de5c9634e65eaec696c9937c626ff3b13a6
Kim Young Jin <wndrhtkfurh@naver.com>
2021-08-24 00:51:39 +0900

    Commit Test

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -31,7 +31,7 @@ public class AppConfig {
 
     @Bean
     public DiscountPolicy discountPolicy(){
-        //return new FixDiscountPolicy(); Test Test Test Test
+        //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
 }
